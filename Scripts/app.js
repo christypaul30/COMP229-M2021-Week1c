ee98ddf80f5efeee4c9008dd6c5f3cb674bbdf78
Christy Paul <christypaul59@gmail.com>
2021-06-03 14:26:34 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+(function(){
+
+    function Start() 
+    {
+        let myVariable = 20;
+        console.log("my number is: " +myVariable);
+        
+        for (var index = 0; index < myVariable; index++) {
+                console.log("your number is" + index);
+            
+            }
+
+
+    }
+
+    window.addEventListener("load", Start);
+
+
+})();
